5e63323793fd686ed7c7af382e61855fa016aadd
BobRazowski <bobongithub@gmail.com>
2015-11-11 10:44:25 +0100

    style ok

--- app/controller.rb
+++ app/controller.rb
@@ -8,5 +8,6 @@ require_relative "controllers/index"
 # require_relative 'data_mapper_setup'
 set :root, File.dirname(__FILE__)
 set :static, true
+set :public_folder, 'public'
 # set :public_folder, File.dirname(__FILE__) + '/public'
 set :views, File.dirname(__FILE__) + "/views"
